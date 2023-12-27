cdd670ddf0c31bfdb7119f0dfc979ae69372f428
lifeisabitch801 <yxq0801@icloud.com>
2023-12-27 23:36:37 +0800

    update

---
+++ src/main/java/r/algorithm/StackVSQueue.java
@@ -0,0 +1,14 @@
+package r.algorithm;
+
+/**
+ * Date:2023/12/24
+ * Author:Jo
+ * Description:栈与队列对比
+ */
+public class StackVSQueue {
+    public static void main(String[] args) {
+        //栈与队列对比
+        //栈 先进后出
+        //队列 先进先出
+    }
+}
