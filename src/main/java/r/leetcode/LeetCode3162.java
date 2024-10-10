1a4ce3a865cb53e8e197660c05a7b54b2d942040
lifeisabitch801 <yxq0801@icloud.com>
2024-10-10 23:36:34 +0800

    LeetCode3162

---
+++ src/main/java/r/leetcode/LeetCode3162.java
@@ -0,0 +1,24 @@
+package r.leetcode;
+
+/**
+ * Date:2024/10/10
+ * Author:Jo
+ * Description: 3162. 优质数对的总数1
+ */
+public class LeetCode3162 {
+    //还在想有什么更好的解法呢，一看题解。。。不就是暴力解吗。。。
+    //浪费时间
+    class Solution {
+        public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+            int res = 0;
+            for (int i : nums1) {
+                for (int j : nums2) {
+                    if (i % j * k == 0) {
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
