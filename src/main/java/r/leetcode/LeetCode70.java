fd02d23e82abe0487b14a508f51b249868a39ea3
lifeisabitch801 <yxq0801@icloud.com>
2024-10-27 00:04:44 +0800

    LeetCode70

---
+++ src/main/java/r/leetcode/LeetCode70.java
@@ -0,0 +1,29 @@
+package r.leetcode;
+
+/**
+ * Date:2024/10/26
+ * Author:Jo
+ * Description:70.爬楼梯
+ */
+public class LeetCode70 {
+    class Solution {
+        public int climbStairs(int n) {
+            if (n == 1) {
+                return 1;
+            }
+            if (n == 2) {
+                return 2;
+            }
+            int fun1 = 1;
+            int fun2 = 2;
+            int fun = 0;
+            for (int i = 3; i <= n; i++) {
+                fun = fun1 + fun2;
+                fun1 = fun2;
+                fun2 = fun;
+            }
+
+            return fun;
+        }
+    }
+}
